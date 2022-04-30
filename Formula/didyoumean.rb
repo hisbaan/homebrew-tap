3d99d50ea4bd0f9385754bfe8717d77535e2feb8
hisbaan <hisbaan@gmail.com>
2022-04-30 12:23:08 -0400

    didyoumean: update formula for new release

--- Formula/didyoumean.rb
+++ Formula/didyoumean.rb
@@ -1,9 +1,9 @@
 class Didyoumean < Formula
     desc "A CLI spelling corrector"
     homepage "https://github.com/hisbaan/didyoumean"
-    url "https://github.com/hisbaan/didyoumean/releases/download/v1.1.1/dym-1.1.1-x86_64-apple-darwin.tar.gz"
-    sha256 "17670467b9ce0119097830cd23dc9b8fc4e00c25adf48d2c771dc3bc98d96ea8"
-    version "1.1.1"
+    url "https://github.com/hisbaan/didyoumean/releases/download/v1.1.2/dym-1.1.2-x86_64-apple-darwin.tar.gz"
+    sha256 "06167f4bd847f86b22440e0e51e7ab2b0ff38efe1e914c0e2650fff0433f229f"
+    version "1.1.2"
 
     def install
         bin.install "dym"
