2272ef783fe5a07fd11706a985b096376ade1a56
hisbaan <hisbaan@gmail.com>
2022-05-13 13:02:31 -0400

    Update didyoumean with manpage and shell completions

--- Formula/didyoumean.rb
+++ Formula/didyoumean.rb
@@ -1,11 +1,15 @@
 class Didyoumean < Formula
     desc "A CLI spelling corrector"
     homepage "https://github.com/hisbaan/didyoumean"
-    url "https://github.com/hisbaan/didyoumean/releases/download/v1.1.2/dym-1.1.2-x86_64-apple-darwin.tar.gz"
-    sha256 "06167f4bd847f86b22440e0e51e7ab2b0ff38efe1e914c0e2650fff0433f229f"
-    version "1.1.2"
+    url "https://github.com/hisbaan/didyoumean/releases/download/v1.1.3/dym-1.1.3-x86_64-apple-darwin.tar.gz"
+    sha256 "eb95bb60a344bd12b6b52e25768e27ab185fb298fdf5e9e9f6ddd43d138d0c34"
+    version "1.1.3"
 
     def install
         bin.install "dym"
+        man1.install "man/dym.1"
+        bash_completion.install "completions/dym.bash"
+        fish_completion.install "completions/dym.fish"
+        zsh_completion.install "completions/_dym"
     end
 end
